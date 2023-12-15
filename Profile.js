6e21e5f586e9b5ae9c4b4c918ba277dfebae0df8
Light1603 <59634280+Light1603@users.noreply.github.com>
2023-12-15 19:55:46 +0700

    Add files via upload

---
+++ Profile.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import './profile.css';
+
+
+const Profile = () => {
+  const [student, setStudent] = useState({});
+  const { uid } = useParams();
+
+ useEffect(() => {
+  axios.get(`http://localhost:3001/profile/${uid}`)
+    .then((response) => {
+      setStudent(response.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching profile data:", error);
+    });
+}, [uid]);
+
+  return (
+    
+    <div className="wrapper">
+      {/* header section starts */}
+      <header>
+        <div className="logo">
+          <p>
+            Tutor<span>Finder</span>.
+          </p>
+        </div>
+        <div className="navbar">
+          <nav>
+            <a href="#home">Home</a>
+            <a href="#sign-up">Sign up</a>
+            <a href="#log-in">Log in</a>
+          </nav>
+        </div>
+      </header>
+      {/* header section ends */}
+
+      {/* main section starts */}
+      <div className="div-8">
+        <div className="div-9">
+          <div className="column">
+            <div className="div-10">Edit profile picture</div>
+          </div>
+          <div className="column-2">
+            <div className="div-11">
+              <div className="div-12">
+                <div className="div-13">Your Student Profile</div>
+                <div className="div-14">UID</div>
+                <div className="div-15">{student.uid}</div>
+                <div className="div-16">Name</div>
+                <div className="div-17">{student.name}</div>
+                <div className="div-18">Phone number</div>
+                <div className="div-19">0123456789</div>
+              </div>
+              <div className="div-20">
+                <div className="div-21">Email</div>
+                <div className="div-22">{student.email}</div>
+                <div className="div-23">Gender</div>
+                <div className="div-24">Male</div>
+                <div className="div-25">Day of birth</div>
+                <div className="div-26">{student.birthdate}</div>
+                <div className="div-27">Update your info</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="div-28">
+        <div className="div-29">
+          <div className="div-30">
+            <div className="div-31">Your website info</div>
+            <div className="div-32">Education Level</div>
+            <div className="div-33">{student.educationLevel}</div>
+          </div>
+          <div className="div-34">
+            <div className="div-35">Subject of Interest</div>
+            <div className="div-36">{student.subjectOfInterest}</div>
+          </div>
+        </div>
+        <div className="div-37">Credit</div>
+        <div className="div-38">{student.credit}$</div>
+      </div>
+      {/* main section ends */}
+
+      {/* footer section starts */}
+      <section className="footer">
+        <div className="footer-content">
+          <Link to="#" >My profile</Link>
+          <Link to="#" >Home</Link>
+          <Link to="#" >About us</Link>
+          <Link to="#" >Private policy</Link>
+        </div>
+      </section>
+      {/* footer section ends */}
+    </div>
+  );
+};
+
+export default Profile;
--- server.js
+++ server.js
@@ -2,43 +2,73 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-const StudentModel = require('./src/models/studentModel')
+const Student = require('./src/models/studentModel')
+const bodyParser = require('body-parser');
+const StudentModel = require('./src/models/studentModel');
+
 
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://yhk8462:2510@cluster0.lt8ygbd.mongodb.net/tutorfinder?retryWrites=true&w=majority")
-.then(() => 
-    console.log('Connected to MongoDB'),
-    app.listen(3001, () => {
-        console.log('Server is running on port 3001');
-    }))
+mongoose.connect("mongodb+srv://yhk8462:2510@cluster0.lt8ygbd.mongodb.net/tutorfinder?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+  });
+})
 .catch((err) => console.log('Failed to connect to MongoDB', err));
 
+async function insert(){
+    await StudentModel.create({
+        name: "asd",
+        email: "asd@gmail.com"
+    });
+};
 
+insert();
 // Routes for Student collection
-app.get('/getStudents', async (req, res) => {
-    StudentModel.find()
-      .then(students => {
-        // console.log(students); // Log the actual data
-        res.json(students);
-      })
-      .catch(err => res.json(err));
-});
+// app.get('/getStudents', async (req, res) => {
+//     StudentModel.find()
+//       .then(students => {
+//         console.log(students); // Log the actual data
+//         res.json(students);
+//       })
+//       .catch(err => res.json(err));
+//   });
 
-app.get('/getStudents/:uid', async (req, res) => {
-    const uid = req.params.uid;
-    StudentModel.findOne({uid: uid})
-      .then(students => {
-        // console.log(students); // Log the actual data
-        res.json(students);
-      })
-      .catch(err => res.json(err));
+// app.post('/postStudents', async (req, res) => {
+
+// });
+
+app.get('/profile/:uid', async (req, res) => {
+    const student = await StudentModel.findOne({ uid: req.params.uid });
+    if (!student) {
+      return res.status(404).send('Student not found');
+    }
+    res.json(student);
   });
   
+  app.put('/update/:uid', async (req, res) => {
+    const updatedStudent = await StudentModel.findOneAndUpdate(
+      { uid: req.params.uid },
+      { $set: req.body },
+      { new: true }
+    );
+  
+    if (!updatedStudent) {
+      return res.status(404).json({ message: 'Student not found' });
+    }
+  
+    res.json(updatedStudent);
+  });
 
-app.post('/postStudents', async (req, res) => {
-
-});
+//Define route for /profile page
+  app.get('/profile', (req, res) => {
+    res.send('This is the profile page');
+  });
 
