49b913f43b43fcea552cb98e204250547c7e0c18
Duongduong98 <70836480+Duongduong98@users.noreply.github.com>
2023-12-15 19:59:29 +0700

    Add files via upload

---
+++ users_homepage.jsx
@@ -0,0 +1,91 @@
+<>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>TutorFinder</title>
+  <link href="css/fontawesome.css" rel="stylesheet" />
+  <link href="css/solid.css" rel="stylesheet" />
+  <link href="index.css" rel="stylesheet" type="text/css" />
+  <div className="wrapper">
+    {/* header section starts */}
+    <header>
+      <div className="logo">
+        <p>
+          Tutor<span>Finder</span>.
+        </p>
+      </div>
+      <div className="navbar">
+        <nav>
+          <a className="quick-link" href="#home">
+            Home
+          </a>
+          <a className="quick-link" href="#home">
+            Profile
+          </a>
+          <a className="quick-link" href="#sign-up">
+            Courses
+          </a>
+          <a className="quick-link" href="#log-in">
+            Schedules
+          </a>
+          <a className="quick-link" href="#log-in">
+            Log Out
+          </a>
+        </nav>
+      </div>
+    </header>
+    {/* header section ends */}
+    {/* main section starts */}
+    {/* main section ends */}
+    {/* footer section starts */}
+    <section className="footer">
+      <div className="footer-content">
+        <a className="quick-link" href="#">
+          My profile
+        </a>
+        <a className="quick-link" href="#">
+          Home
+        </a>
+        <a className="quick-link" href="#">
+          About us
+        </a>
+        <a className="quick-link" href="#">
+          Private policy
+        </a>
+      </div>
+    </section>
+    {/* footer section ends */}
+    {/* welcome message */}
+    <h1>
+      Effectively leverage <br /> your study <br /> with the best tutors
+    </h1>
+  </div>
+  <div className="container">
+    {/* First box */}
+    <div className="box">
+      <p className="box-content">200+ tutors with intensive experience</p>
+    </div>
+    {/* Second box */}
+    <div className="box">
+      <p className="box-content">20+ subjects</p>
+    </div>
+    {/* Box 3 */}
+    <div className="box">
+      <p className="box-content">95% of 5-star tutor reviews</p>
+    </div>
+    {/* Box 4 */}
+    <div className="box">
+      <p className="box-content">Another box content</p>
+    </div>
+  </div>
+  {/* Search bar container */}
+  <div className="search-container">
+    {/* Search bar */}
+    <input
+      type="text"
+      className="search-bar"
+      placeholder="Type your courses you want to study here!"
+    />
+    {/* Search button */}
+    <button className="search-button">Search</button>
+  </div>
+</>
