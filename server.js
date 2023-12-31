333fee3a0817444d9f46fe919f33774e44b081bf
numi8462 <yhk8462@naver.com>
2023-12-31 20:22:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -171,3 +171,22 @@ app.get('/getUser/:uid', async (req, res) => {
   })
   .catch(err => res.json(err));
 });
+
+// API endpoint to delete a course by ID
+app.delete('/courses/:cid', async (req, res) => {
+  const courseId = req.params.cid;
+
+  try {
+    // Find and delete the course by ID
+    const result = await Course.findByIdAndDelete(courseId);
+    
+    if (result) {
+      res.status(204).end(); // Course deleted successfully
+    } else {
+      res.status(404).json({ error: 'Course not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting course:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
