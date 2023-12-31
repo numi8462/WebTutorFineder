aeea5b5615b0a723f7d35f086e8fa8a3b919b285
numi8462 <yhk8462@naver.com>
2023-12-31 20:20:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -143,26 +143,6 @@ app.post('/postCourse', async (req, res) => {
   }
 });
 
-// // API endpoint to delete a course by ID
-// app.delete('/courses/:cid', async (req, res) => {
-//   const courseId = req.params.cid;
-
-//   try {
-//     // Find and delete the course by ID
-//     const result = await Course.findByIdAndDelete(courseId);
-    
-//     if (result) {
-//       res.status(204).end(); // Course deleted successfully
-//     } else {
-//       res.status(404).json({ error: 'Course not found' });
-//     }
-//   } catch (error) {
-//     console.error('Error deleting course:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-
 //returns both student and tutor
 app.get('/users', async (req, res) => {
   const uid = req.params.uid;
@@ -192,4 +172,24 @@ app.get('/getUser/:uid', async (req, res) => {
     }
   })
   .catch(err => res.json(err))
-})
+});
+
+
+// API endpoint to delete a course by ID
+app.delete('/courses/:cid', async (req, res) => {
+  const courseId = req.params.cid;
+
+  try {
+    // Find and delete the course by ID
+    const result = await Course.findByIdAndDelete(courseId);
+    
+    if (result) {
+      res.status(204).end(); // Course deleted successfully
+    } else {
+      res.status(404).json({ error: 'Course not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting course:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
