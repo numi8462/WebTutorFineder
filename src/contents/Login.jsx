bb49ee52b0048cefcba1dd0b929b05c66b8fcce3
numi8462 <yhk8462@naver.com>
2024-01-06 14:40:11 +0700

    .
    .

--- src/contents/Login.jsx
+++ src/contents/Login.jsx
@@ -15,24 +15,39 @@ export const Login = (props) => {
 
     async function handleSubmit(e) {
         e.preventDefault();
-        // console.log(email);
 
         try {
-            setError("")
-            setLoading(true)
-            await login(email, pass)
-            navigate('/studentDashboard');
-        } catch {
-            console.log(email)
-            console.log(pass)
-            setError("Failed to login")
-        }
-        setLoading(false)
-        console.log(email);
+            setError("");
+            setLoading(true);
+            const userCredential = await login(email, pass); // Assuming login returns userCredential
+            const uid = userCredential.user.uid; // Get uid from userCredential
+            console.log(uid)
+            // Fetch user by uid
+            const response = await fetch(`http://localhost:3001/getUser/${uid}`);
+            const data = await response.json();
+            console.log("role is: "+data.role)
+            // Navigate based on user's role
+            if (data.role === 'student') {
+                navigate('/studentDashboard');
+            } else if (data.role === 'tutor') {
+                navigate('/tutorDashboard');
+            } else {
+                setError("Failed to determine user role");
+            }
 
+        } catch (error) {
+            setError(`Failed to login: ${error.message}`);
+            console.error(error);
+        }
+
+        setLoading(false);
     }
+
     const handleSwitch = () => {
         navigate('/register');
+    };
+    const handleSwitchTut = () => {
+        navigate('/registerTutor');
     };
 
     return (
