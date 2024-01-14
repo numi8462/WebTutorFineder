d24cca6e64086249dc8e7625ddbc7ee4bcf45475
numi8462 <yhk8462@naver.com>
2024-01-14 16:44:06 +0700

    .

--- src/contents/student/Profile.jsx
+++ src/contents/student/Profile.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../authentication/AuthContext';
+import { ToastContainer, toast } from 'react-toastify';
 import firebase from "firebase/compat/app";
 import axios from 'axios';
 import '../../index.css';
+import { useNavigate } from "react-router-dom";
 
 export const Profile = (props) => {
   const [student, setStudent] = useState({});
@@ -13,22 +13,75 @@ export const Profile = (props) => {
   // const { uid } = useParams();
   const [uid, setUid] = useState('')
 
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      setUid(user.uid);
-    }
-  });
 
- useEffect(() => {
-  axios.get(`http://localhost:3001/profile/${uid}`)
-    .then((response) => {
-      setStudent(response.data);
-    })
-    .catch((error) => {
-      console.error("Error fetching profile data:", error);
-    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/profile/${uid}`);
+        if (response.data.user === "student") {
+          setStudent(response.data);
+        } else {
+          setTutor(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching profile data:", error);
+      }
+    };
+
+    const handleAuthStateChange = (user) => {
+      if (user) {
+        setUid(user.uid);
+        fetchData(); // Fetch student data when user logs in
+      }
+    };
+
+    const authUnsubscribe = firebase.auth().onAuthStateChanged(handleAuthStateChange);
+
+    return () => {
+      // Cleanup the auth state subscription
+      authUnsubscribe();
+    };
   }, [uid]);
 
+  const handleFormSubmit = async (e) => {
+    e.preventDefault(); 
+  }
+  const updateStudentInfo = async (updatedData) => {
+    try {
+      await axios.post(`http://localhost:3001/updateStudent/${uid}`, updatedData);
+    } catch (error) {
+      console.error("Error updating profile data:", error);
+    }
+  };
+
+  const handleInputChange = (field, value) => {
+ 
+    setStudent((prevStudent) => ({
+      ...prevStudent,
+      [field]: value,
+    }));
+    
+  };
+
+  const handleUpdateClick = async () => {
+    const updatedData = {
+      name: student.name,
+      phonenumber: student.phonenumber,
+      email: student.email,
+      gender: student.gender,
+      birthdate: student.birthdate,
+    };
+
+    try {
+      await updateStudentInfo(updatedData);
+      alert('Profile updated successfully!');
+      window.location.reload();
+    } catch (error) {
+      console.error("Error updating profile data:", error);
+      toast.error('Error updating profile');
+    }
+  };
   return (
     <div>
             <meta charSet="UTF-8" />
@@ -45,7 +98,7 @@ export const Profile = (props) => {
                 <div className="sidebar-menu">
                     <ul>
                         <li>
-                            <a><span className="fa-solid fa-list-check"></span>
+                            <a onClick={() => navigate('/studentDashboard')}><span className="fa-solid fa-list-check"></span>
                             <span>My Courses</span></a>
                         </li>
                         <li>
@@ -85,49 +138,124 @@ export const Profile = (props) => {
             </header>
         
             <main className='main'>
-            
+        <form onSubmit={handleFormSubmit}>
           <div className="upper">
-          <div className="p-pic-container">
-            <p>Edit profile picture</p>
-          </div>
           <div className="change-info">
             <div className="change-info-inside">
+              <div className="p-pic-container">
+                <p>Edit profile picture</p>
+              </div>
               <div className="left-info">
                 <div className="head">Your Student Profile</div>
                 <div className="stable">UID</div>
-                <div className="changed">{student.uid}</div>
+                <div className="changed">
+                  <input name="email" id="email" placeholder={student.uid} type="email"/>
+                </div>
                 <div className="stable">Name</div>
-                <div className="changed">{student.name}</div>
+
+                <div className="input-group">
+                  <input
+                    name="name"
+                    id="name"
+                    placeholder="Enter your name"
+                    type="text"
+                    value={student.name}
+                    onChange={(e) => handleInputChange("name", e.target.value)}
+                  />
+                </div>
                 <div className="stable">Phone number</div>
-                <div className="changed">0123456789</div>
-              </div>
+                  <div className="input-group">
+                    <input
+                      name="phone"
+                      id="phone"
+                      placeholder="Enter your phone number"
+                      type="text"
+                      value={student.phonenumber}
+                      onChange={(e) => handleInputChange("phonenumber", e.target.value)}
+                    />
+                  </div>
+                </div>
               <div className="right-info">
-                <div className="div-21">Email</div>
-                <div className="div-22">{student.email}</div>
-                <div className="div-23">Gender</div>
-                <div className="div-24">Male</div>
-                <div className="div-25">Day of birth</div>
-                <div className="div-26">{student.birthdate}</div>
-                <button className="btn" onClick={() => navigate('/updateProfile')}>Update your info</button>
+
+              <div className="stable">Email</div>
+                <div className="input-group">
+                  <input
+                    name="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    type="email"
+                    value={student.email}
+                    onChange={(e) => handleInputChange("email", e.target.value)}
+                  />
+                </div>
+                <div className="stable gender">Gender</div>
+                  <div className='option'>
+                    <input
+                      type="radio"
+                      id="male"
+                      name="gender"
+                      value="male"
+                      checked={student.gender === "male"}
+                      onChange={() => handleInputChange("gender", "male")}
+                    />
+                    <label htmlFor="male">Male</label>
+                  </div>
+                  <div className='option'>
+                    <input
+                      type="radio"
+                      id="female"
+                      name="gender"
+                      value="female"
+                      checked={student.gender === "female"}
+                      onChange={() => handleInputChange("gender", "female")}
+                    />
+                    <label htmlFor="female">Female</label>
+                  </div>
+                  <div className='option'>
+                    <input
+                      type="radio"
+                      id="other"
+                      name="gender"
+                      value="other"
+                      checked={student.gender === "other"}
+                      onChange={() => handleInputChange("gender", "other")}
+                    />
+                    <label htmlFor="other">Other</label>
+                  </div>
+                  <div className="stable">Day of birth</div>
+                  <div className="changed">
+                    <input
+                      type="date"
+                      name="dob"
+                      placeholder="dd-mm-yyyy"
+                      defaultValue={student.birthdate}
+                      min="1997-01-01"
+                      max="2030-12-31"
+                      onChange={(e) => handleInputChange("birthdate", e.target.value)}
+                    />
+                  </div>
+                  <button className="btn" type='onSubmit' onClick={handleUpdateClick}>
+                    Update your info
+                  </button>
               </div>
             </div>
           </div>
         </div>
-      
+      </form>
       <div className="web-info">
         <div className="div-29">
           <div className="div-30">
-            <div className="div-31">Your website info</div>
-            <div className="div-32">Education Level</div>
-            <div className="div-33">{student.educationLevel}</div>
+            <div className="div-31">Your Student info</div>
+            <div className="stable"><h4>Education Level</h4></div>
+            <div className="changed">{student.educationLevel}</div>
           </div>
           <div className="div-34">
-            <div className="div-35">Subject of Interest</div>
-            <div className="div-36">{student.subjectOfInterest}</div>
+            <div className="stable"><h4>Subject of Interest</h4></div>
+            <div className="changed">{student.subjectOfInterest}</div>
           </div>
         </div>
-        <div className="div-37">Credit</div>
-        <div className="div-38">{student.credit}$</div>
+        <div className="stable"><h4>Credit</h4></div>
+        <div className="changed">{student.credit}$</div>
       </div>
 
             </main>
@@ -135,4 +263,3 @@ export const Profile = (props) => {
     </div>
   );
 };
-
